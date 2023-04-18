80040a3ad2d6d4f9044995adb3e6af698b2b0144
Artur <128506353+Artur-iq@users.noreply.github.com>
2023-04-18 19:53:48 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,96 +1,22 @@
-from aiogram import types, Bot, Dispatcher
 from aiogram.utils import executor
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from decouple import config
-
+from config import dp
 import logging
 
+from handlers import client
+from handlers import callback
+from handlers import admin
+from handlers import fsmadmin
+# from handlers import extra
+fsmadmin.register_handlers_fsm(dp)
+client.register_handlers_client(dp)
+admin.register_handlers_admin(dp)
+callback.register_handler_callback(dp)
 
 
 
-
-TOKEN = config("TOKEN")
-
-bot = Bot(token=TOKEN)
-dp =  Dispatcher(bot=bot)
-
-@dp.message_handler(commands=['mem'])
-async def process_photo_command(message: types.Message):
-    with open("photo.jpg", 'rb') as photo:
-        await bot.send_photo(chat_id=message.chat.id, photo = photo)
-
-
-@dp.message_handler(commands=['quiz'])
-async def quiz_1(message: types.Message):
-    markup = InlineKeyboardMarkup()
-    button_1 = InlineKeyboardButton("NEXT", callback_data="quiz_1_button")
-    markup.add(button_1)
-
-    question = "Какая самая высокая гора в мире?"
-    answer = [
-        "Джомолунгма",
-        "Лхоцзе",
-        "Чогори",
-        "Канченджанга",
-
-    ]
-
-    await bot.send_poll(
-        chat_id=message.from_user.id,
-        question=question,
-        options=answer,
-        is_anonymous=False,
-        type="quiz",
-        correct_option_id=0,
-        explanation="ОООЙЙЙ ЁЁЁЁЁ, ну ты даешь",
-        open_period=10,
-        reply_markup=markup
-    )
-    # await message.answer_poll()
-
-
-@dp.callback_query_handler(text="quiz_1_button")
-async def quiz_2(call: types.CallbackQuery):
-    question = "Какая самая популярная манга?"
-    answer = [
-        "Жемчуг дракона",
-        "Golgo 13",
-        "One Piece",
-        "Наруто",
-        "Детектив Конан",
-        "Black Jack",
-    ]
-
-    await bot.send_poll(
-        chat_id=call.from_user.id,
-        question=question,
-        options=answer,
-        is_anonymous=False,
-        type="quiz",
-        correct_option_id=2,
-        explanation="Не быть тебе королем пиратовn",
-        open_period=10,
-    )
-
-@dp.message_handler()
-async def message_handler(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id, text=message.text)
-    try:
-        number = int(message.text)
-        result = number ** 2
-        await message.answer(str(result))
-        await message.reply("Это число которое возводится в квадрат!")
-    except ValueError:
-        pass
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id, text=message.text)
-
+# extra.register_handler_extra(dp)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=True)
-
